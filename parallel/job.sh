27a0576053e290483ee37239ed36e175326785dc
Rodrigo Jose Gonzalez Hernandez <rodrigojosegonzalez@login2.ls6.tacc.utexas.edu>
2024-12-04 17:52:36 -0600

    added the job.sh file for TACC

---
+++ parallel/job.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH -N 1
+#SBATCH -n 128
+#SBATCH -o coulumb_teset.%j.out
+#SBATCH -J coulumb_test
+#SBATCH -p normal
+#SBATCH -A DMS23021
+#SBATCH -t 5:00:00
+
+#SBATCH --mail-user=rodrigogonzalez@utexas.edu
+#SBATCH --mail-type=begin
+#SBATCH --mail-type=end
+
+conda init
+conda activate petrov_env
+python3 parallel.py
