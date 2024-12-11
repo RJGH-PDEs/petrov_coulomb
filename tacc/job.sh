4858de835e6023fb02a1206edad741be84728d47
Rodrigo Jose Gonzalez Hernandez <rodrigojosegonzalez@login2.ls6.tacc.utexas.edu>
2024-12-11 14:20:43 -0600

    latest from tacc, including some results

--- tacc/job.sh
+++ tacc/job.sh
@@ -2,16 +2,15 @@
 
 #SBATCH -N 1
 #SBATCH -n 128
-#SBATCH -o coulumb_test.%j.out
-#SBATCH -J coulumb_test
+#SBATCH -o coulumb_recompute.%j.out
+#SBATCH -J coulumb_recompute
 #SBATCH -p normal
 #SBATCH -A DMS23021
-#SBATCH -t 23:50:00
+#SBATCH -t 02:00:00
 
 #SBATCH --mail-user=rodrigogonzalez@utexas.edu
 #SBATCH --mail-type=begin
 #SBATCH --mail-type=end
 
 conda init
-conda activate petrov_env
-python3 ../parallel/parallel.py
+python3 ../parallel/recompute.py
